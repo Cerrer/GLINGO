f8cf81241c2b09e44c9fd6de1f1aea1a46024a4d
Cerrer <Cerrer@hotmail.com>
2023-12-25 21:09:45 +0300

    test_G

---
+++ PostUrl/appSite/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+class PageHome(View):
+    template_home = 'appSite/index.html'
+    def get(self, request):
+        ses = request.session.get('hist', False)
+        if ses :
+            return HttpResponse("Сессия есть!")
+        else:
+            request.session['hist'] = "ok"
+            return HttpResponse("Сессии нету!")
+        # return render(request, self.template_home)
+    def post(self, request):
+        # print(request.POST['number'])
+        return HttpResponse(request.POST['number'])
+
