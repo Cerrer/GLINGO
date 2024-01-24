98420d3be424061ce8a2976a6822957ae1c55f95
Cerrer <Cerrer@hotmail.com>
2024-01-24 21:04:30 +0300

    Documents
    Создание документов

--- GLINGO_2/appSites_2/views.py
+++ GLINGO_2/appSites_2/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from .models import ContentBanner
+from .models import ContentBanner, AboutMe, Documents
 
 class HomePage(View):
     def get(self, request):
+        # print(AboutMe.objects.first())   # вытягивает первый объект
+        # print(AboutMe.objects.all()[1])    # вытягивает 2 объект
         data = {
             'banner':ContentBanner.objects.order_by("number"),
+            'about_me':AboutMe.objects.first(),
+            'documents':Documents.objects.all()
         }
         return render(request, 'appSites_2/home/index.html', data)
